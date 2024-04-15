6453e51507736ceebc70e264e0873c477a333a64
Sintu Kumar <kumarsintu499@gmail.com>
2024-04-15 18:47:33 +0530

    add sort method

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,19 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    featured: true,
-  });
+  // const products = await Product.find({
+  //   featured: true,
+  // });
+
+  // product short
+  const products = await Product.find({}).sort("price name");
 
   res.status(200).json({ products, nbHits: products.length });
   // res.status(200).json({ msg: "Products testing route" });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true ? true : false";
@@ -18,10 +21,23 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
+  // regex used because name has space
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  const products = await Product.find(queryObject);
+
+  console.log(queryObject);
+
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
   // res.status(200).json({ msg: "Products route" });
 };
